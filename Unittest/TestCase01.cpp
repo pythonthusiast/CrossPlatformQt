fb4bb593f702b78cbe6282eb600c98fbdfb5df03
swdev <swdev.bali@gmail.com>
2014-02-05 05:12:53 +0700

    test case

---
+++ Unittest/TestCase01.cpp
@@ -0,0 +1,9 @@
+#include "TestCase01.h"
+
+void TestCase01::testAuth()
+{
+    QCOMPARE("eko", "eko");
+}
+
+QTEST_MAIN(TestCase01)
+#include "TestCase01.moc"
---
+++ Unittest/TestCase01.h
@@ -0,0 +1,12 @@
+#ifndef TEST_CASE_01
+#define TEST_CASE_01
+#include <QtTest/QtTest>
+
+class TestCase01: public QObject
+{
+    Q_OBJECT
+private slots:
+    void testAuth();  
+};
+
+#endif
