eae5b74de1c37c7cc45c969f97d53376e4401e1c
Eko S. Wibowo <swdev.bali@gmail.com>
2014-02-11 07:02:43 +0700

    testing Komodo IDE for unit test : must rename file name to test_*

--- 02-Unittest/PyUnitTesting/test_features01.py
+++ 02-Unittest/PyUnitTesting/test_features01.py
@@ -5,6 +5,10 @@ class TestCase01(unittest.TestCase):
     def testAuth(self):
         auth = Auth()
         self.assertEqual(auth.doLogin("eko", "oke"), True)
+        
+    def testError(self, ):
+        self.assertEqual(5,10);
+    
 
 if __name__ == "__main__":
     unittest.main()
