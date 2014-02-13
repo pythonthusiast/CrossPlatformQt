dfdc96dc6e86e52daf741a1471d6249102f25c7c
swdev <swdev.bali@gmail.com>
2014-02-13 16:45:41 +0700

    completed!

--- 03-SQL/CppQt/auth.cpp
+++ 03-SQL/CppQt/auth.cpp
@@ -3,6 +3,12 @@
 bool Auth::doLogin(QString name, QString pass)
 {
     QSqlQuery query;
-    query.exec("SELECT");
-	return name == "eko";
+    query.prepare("select id from qtapp_users where username = :username and password = md5(:password)");
+    query.bindValue(":username", name);
+    query.bindValue(":password", pass);
+    query.exec();
+    if(query.next()){
+        return true;
+    }
+    return false;
 }
