c6448b7bac82ae3dd2a561ff98be1dbbf25a767d
swdev <swdev.bali@gmail.com>
2014-02-14 21:07:01 +0700

    finish login feature of C++ Qt

--- 03-SQL/CppQt/auth.cpp
+++ 03-SQL/CppQt/auth.cpp
@@ -1,11 +1,13 @@
 #include "auth.h"
 #include <QtSql>
+#include "helper.h"
+
 bool Auth::doLogin(QString name, QString pass)
 {
     QSqlQuery query;
-    query.prepare("select id from qtapp_users where username = :username and password = md5(:password)");
+    query.prepare("select id from qtapp_users where username = :username and password = :password");
     query.bindValue(":username", name);
-    query.bindValue(":password", pass);
+    query.bindValue(":password", Helper::computeHash(pass));
     query.exec();
     if(query.next()){
         return true;
