493aae4e61e1e28fb715c8e6a425aace5c8ef617
Eko S. Wibowo <swdev.bali@gmail.com>
2014-02-15 12:49:54 +0700

    pyqt solved!

--- 03-SQL/PyQt/auth.py
+++ 03-SQL/PyQt/auth.py
@@ -1,3 +1,13 @@
+from PyQt4.QtSql import *
+import helper
+
 class Auth:
     def doLogin(self, username, password):
-        return True
+        query = QSqlQuery()
+        query.prepare("select id from qtapp_users where username = :username and password = :password")
+        query.bindValue(":username", username)
+        query.bindValue(":password", helper.computeHash(password))
+        query.exec_()
+        if query.next():
+            return True
+        return False
--- 03-SQL/PyQt/test_features01.py
+++ 03-SQL/PyQt/test_features01.py
@@ -1,15 +1,22 @@
 import unittest
+from PyQt4.QtCore import QCoreApplication
 from auth import Auth
+import helper
+import sys
 
 class TestCase01(unittest.TestCase):
+    def testConnect(self):
+        qApp = QCoreApplication(sys.argv)
+        self.assertTrue(helper.dbConnect())
+
+    def testMd5(self):
+        self.assertEqual(helper.computeHash("password"),"5f4dcc3b5aa765d61d8327deb882cf99")
+
     def testAuth(self):
+        qApp = QCoreApplication(sys.argv)
+        helper.dbConnect()
         auth = Auth()
-        self.assertEqual(auth.doLogin("eko", "oke"), True)
-        
-    def testError(self, ):
-        self.assertEqual(5,10);
-    
+        self.assertEqual(auth.doLogin("eko", "password"), True)
 
 if __name__ == "__main__":
     unittest.main()
-
