1de152d1aa2327e3ee21921a614b9bc063ded3e8
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2014-02-20 14:09:33 +0700

    using pyside on osx

--- 04-UI/PyQtUi/test_features01.py
+++ 04-UI/PyQtUi/test_features01.py
@@ -1,16 +1,19 @@
 import unittest
+from PyQt4.QtCore import QCoreApplication
 from auth import Auth
 import helper
-
+import sys
 
 class TestCase01(unittest.TestCase):
     def testConnect(self):
+        qApp = QCoreApplication(sys.argv)
         self.assertTrue(helper.dbConnect())
 
     def testMd5(self):
         self.assertEqual(helper.computeHash("password"),"5f4dcc3b5aa765d61d8327deb882cf99")
 
     def testAuth(self):
+        qApp = QCoreApplication(sys.argv)
         helper.dbConnect()
         auth = Auth()
         self.assertEqual(auth.doLogin("eko", "password"), True)
